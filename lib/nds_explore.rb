d95a998c28d090ec8cd7c5c1cc3705ee0e65188d
Michael Calkins <m.calkins576@gmail.com>
2019-10-18 20:06:34 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,9 +11,9 @@ end
 def print_first_directors_movie_titles
   
   movie_index = 0
-  while movie_index < directors_database[0][movies].length do
+  while movie_index < directors_database[0][:movies].length do
     title_index = 0
-    while titel_index < directors_database[0][movie_index].length do
+    while titel_index < directors_database[0][:movies].length do
       puts directors_database[row_index][element_index]
     titel_index += 1
   end
